bc35ccee57d0062791009fbacbcf41dde330d743
Abdur Rehman Kazim <abdurrehmankazim68@gmail.com>
2023-12-02 21:54:18 +0500

    working

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
         throw new UnauthenticatedError('Authentication invalid')
     }
 
-    const token = autHeader.split(' ')[1]
+    const token = authHeader.split(' ')[1]
 
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
