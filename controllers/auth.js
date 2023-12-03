1a3e680e07487b4f4fea478ff75cd7a5dfec7b06
Abdur Rehman Kazim <abdurrehmankazim68@gmail.com>
2023-12-03 20:10:53 +0500

    working

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,8 @@ const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, UnauthenticatedError} = require('../errors')
 
-const register = (req, res) => {
-    const user = User.create({ ...req.body })
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body })
     const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
@@ -21,7 +21,6 @@ const login = async (req, res) => {
     if (!isPasswordCorrect) {
         throw new UnauthenticatedError('Invalid credentials')
     }
-
     const token = user.createJWT()
     res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,13 +1,20 @@
-const getAllJobs = (req, res) => {
-    res.json({ msg: 'all jobs'})
+const Job = require("../models/Job")
+const { StatusCodes } = require("http-status-codes")
+const {BadRequestError, NotFoundError} = require("../errors")
+
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count:jobs.length})
 }
 
 const getJob = (req, res) => {
     res.json({ msg: 'single job'})
 }
 
-const createJob = (req, res) => {
-    res.json({ msg: 'create job'})
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create({...req.body})
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = (req, res) => {
